b18732705de6aab82cd54756ab567539a5f87854
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2013-09-11 14:44:32 +0200

    fix directory name in web bootstrap script location

--- web-bootstrap.sh
+++ web-bootstrap.sh
@@ -2,5 +2,5 @@ yum -y install httpd php
 chkconfig httpd on
 /etc/init.d/httpd start
 cd /var/www/html
-wget http://aws-emea.info.s3.amazonaws.com/resources/latency-demo/web-examplefiles.zip
+wget http://aws-emea.info.s3.amazonaws.com/resources/route53-lbr/web-examplefiles.zip
 unzip web-examplefiles.zip
