a3fef8f7d944ffbc05151154eaa2cfd868b18e6a
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2014-05-11 14:41:51 +0200

    add cfn-signal for error handling

--- web-bootstrap.sh
+++ web-bootstrap.sh
@@ -1,6 +1,42 @@
-yum -y install httpd php
-chkconfig httpd on
-/etc/init.d/httpd start
+#!/bin/bash -x
+
+# send std out to log file
+exec &>> /home/ec2-user/bootstrap.log
+
+function error_exit() {
+    echo "{\"Reason\": \"$1\"}"
+    exit $2
+}
+
+echo "Installing Apache's HTTPD and PHP"
+RETURN_CODE = $(/usr/bin/yum -y install httpd php)
+if [ $RETURN_CODE -ne 0 ]
+then
+    error_exit "yum install httpd php failed" $RETURN_CODE
+fi
+
+RETURN_CODE=$(/sbin/chkconfig httpd on)
+if [ $RETURN_CODE -ne 0 ]
+then
+    error_exit "chkconfig failed" $RETURN_CODE
+fi
+
+RETURN_CODE=$(/etc/init.d/httpd start)
+if [ $RETURN_CODE -ne 0 ]
+then
+    error_exit "httpd start failed" $RETURN_CODE
+fi
+
 cd /var/www/html
-wget http://aws-emea.info.s3.amazonaws.com/resources/route53-lbr/web-examplefiles.zip
-unzip web-examplefiles.zip
+RETURN_CODE=$(wget http://aws-emea.info.s3.amazonaws.com/resources/route53-lbr/web-examplefiles.zip)
+if [ $RETURN_CODE -ne 0 ]
+then
+    error_exit "download sample web app failed" $RETURN_CODE
+fi
+
+RETURN_CODE=$(unzip web-examplefiles.zip)
+if [ $RETURN_CODE -ne 0 ]
+then
+    error_exit "unzip web app failed" $RETURN_CODE
+fi
+
